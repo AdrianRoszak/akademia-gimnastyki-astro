521ca91bf95abe4409c88bcdbf6d147192545312
Weirdo <adroszak@weirdo.codes>
2025-08-20 17:31:36 +0200

    feat: add nested navigation

---
+++ src/data/get-activities-nav.ts
@@ -0,0 +1,23 @@
+import { sanityClient } from 'sanity:client';
+
+export type ActivityNavItem = {
+	name: string;
+	slug: string;
+};
+
+export async function getActivitiesNav(): Promise<ActivityNavItem[]> {
+	const query = `*[_type == "activity_item"] | order(activity_item_name asc) {
+		activity_item_name,
+		activity_item_slug {
+			current
+		}
+	}`;
+
+	const data = await sanityClient.fetch(query);
+
+	// @ts-ignore
+	return data.map((item) => ({
+		name: item.activity_item_name,
+		slug: item.activity_item_slug.current,
+	}));
+}
